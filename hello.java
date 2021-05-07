f289630fa4f5b7a51efe900db7d4ef1c3c699b50
zhongbaol <2018191080@qq.com>
2021-05-07 15:47:02 +0800

    15:46

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
 //hello world
 public class hello {
 
-    public static void main(){
-        System.out.println("Hello, i am ubantuCode!");
-        System.out.println("Hello ,i am windowsCode!");
+    public static void main(String[] args){
+        //System.out.println("Hello, i am ubantuCode!");
+       // System.out.println("Hello ,i am windowsCode!");
         System.out.println("Hello ,i am Code!");
     }
 }
